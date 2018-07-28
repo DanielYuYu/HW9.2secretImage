20467beef126322a6a1f97fad629fb90b12ae939
Daniel <daniel@macbook-pro.local>
2018-07-28 22:38:45 +0800

    Initial Commit

---
+++ HW9.2secretImage/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HW9.2secretImage
+//
+//  Created by Daniel on 2018/7/28.
+//  Copyright © 2018年 Daniel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
