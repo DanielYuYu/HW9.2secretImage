78cb0a500d852a93f57b330c05445f9706c1581a
Daniel <daniel@macbook-pro.local>
2018-07-28 23:04:06 +0800

    first commit

--- HW9.2secretImage/ViewController.swift
+++ HW9.2secretImage/ViewController.swift
@@ -12,9 +12,39 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        changeView.backgroundColor = UIColor.red
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    @IBOutlet weak var changeView: UIView!
+    var number = 0
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        number = number + 1
+        
+        switch number % 7 {
+        case 0:
+            changeView.backgroundColor = UIColor.red
+        case 1:
+            changeView.backgroundColor = UIColor.orange
+        case 2:
+            changeView.backgroundColor = UIColor.yellow
+        case 3:
+            changeView.backgroundColor = UIColor.green
+        case 4:
+            changeView.backgroundColor = UIColor.blue
+        case 5:
+            changeView.backgroundColor = UIColor.purple
+        case 6:
+            changeView.backgroundColor = UIColor.brown
+        default:
+            changeView.backgroundColor = UIColor.black
+            break
+        }
+        
+        print(number)
 
+    }
+    
+    
 }
 
